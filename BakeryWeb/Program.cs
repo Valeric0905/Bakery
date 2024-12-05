31f0f7e484985b67e662fe1ebd52d6dfef0b0adf
Valeryia <provance.minsk@gmail.com>
2024-12-05 15:37:13 -0500

    Added Identity Register

--- BakeryWeb/Program.cs
+++ BakeryWeb/Program.cs
@@ -1,7 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using BakeryWeb.Data;
+using BakeryWeb.Areas.Identity.Data;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("BakeryWebDBContextConnection") ?? throw new InvalidOperationException("Connection string 'BakeryWebDBContextConnection' not found.");
+
+builder.Services.AddDbContext<BakeryWebDBContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<BakeryWebDBContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireUppercase = false;
+});
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -19,9 +35,11 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
