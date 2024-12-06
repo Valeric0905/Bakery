73ce5ac24b29cfff922c8a20b5373e52c0c632fa
Valeryia <provance.minsk@gmail.com>
2024-12-05 19:34:05 -0500

    Added Login and changed Logout button

--- BakeryWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BakeryWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -14,19 +14,23 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using BakeryWeb.Areas.Identity.Data;
 
 namespace BakeryWeb.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        // Updated constructor to include UserManager
+        public LoginModel(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, ILogger<LoginModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
         }
+
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
